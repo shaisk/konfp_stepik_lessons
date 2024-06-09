4808ca155d654e5e8faabba436c6c9fdcf542310
shaisk <shaisikama@gmail.com>
2024-06-09 19:44:28 +0300

    Add base_class and main_page

---
+++ pages/main_page.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+
+
+class Main_page(Base):
+    url = 'https://konf-p.ru'
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
